cea5937da7048da8b3984e44bc5c7b740fcd7adb
Guh <literallyguh@gmail.com>
2026-03-02 22:24:13 +0100

    auto: 2026-03-02 21:24:13

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require('express');
 const { createClient } = require('@supabase/supabase-js');
 const path = require('path');
+const fs   = require('fs');
 
 const app  = express();
 const PORT = process.env.PORT || 3000;
